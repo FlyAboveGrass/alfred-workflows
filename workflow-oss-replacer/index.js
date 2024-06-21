10b8891a069c39372745364c33b4314f8942d457
yangjiajian <yangjiajian@supermonkey.com.cn>
2024-06-21 14:34:25 +0800

    feat: oss链接替换

---
+++ workflow-oss-replacer/index.js
@@ -0,0 +1,22 @@
+import clipboardy from 'clipboardy'
+import alfy from 'alfy'
+
+let content = clipboardy.readSync();
+if (content.startsWith('oss://img-supermonkey/')) {
+  let newContent = content.replace('oss://img-supermonkey/', 'https://img.supermonkey.com.cn/webapp');
+  alfy.output([
+    {
+      title: newContent,
+      arg: newContent,
+    },
+  ]);
+
+  clipboardy.writeSync(newContent);
+} else {
+  alfy.output([
+    {
+      title: '未找到oss链接' + content,
+      arg: content,
+    },
+  ]);
+}
